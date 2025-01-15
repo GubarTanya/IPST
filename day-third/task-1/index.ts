705f34ea03e7523c875f5cf16a08c69fec96f42e
tanya22222222554646543469 <147590403+tanya22222222554646543469@users.noreply.github.com>
2025-01-15 13:42:34 +0700

    add day3

---
+++ day-third/task-1/index.ts
@@ -0,0 +1,12 @@
+function delayPromise(delay: number, message: string): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(message);
+        }, delay);
+    });
+}
+
+// Пример использования:
+delayPromise(5000, "Hi").then((result) => {
+    console.log(result);
+});
